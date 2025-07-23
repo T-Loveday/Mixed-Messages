1fe2dea9c5b774f8d2cec7ae629add51153218e2
Loveday99 <Trevor.Loveday99@Gmail.com>
2025-07-23 17:21:29 -0400

    Added multiple jokes to MixedMessages and set up the html file to display them.

---
+++ MixedMessages.js
@@ -0,0 +1,47 @@
+
+function joke1() {
+    return "Why don't scientists trust atoms? Because they make up everything!";
+}
+function joke2() {
+    return "Why did the scarecrow win an award? Because he was outstanding in his field!";
+}
+function joke3() {
+    return "Why don't programmers like nature? It has too many bugs!";
+}
+function joke4() {
+    return "Why do we never tell secrets on a farm? Because the potatoes have eyes and the corn has ears!";
+}
+function joke5() {
+    return "Why did the bicycle fall over? Because it was two-tired!";
+}
+function joke6() {
+    return "Why did the math book look sad? Because it had too many problems!";
+}
+function joke7() {
+    return "Why was the computer cold? It left its Windows open!";
+}
+function joke8() {
+    return "Why did the coffee file a police report? It got mugged!";
+}
+function joke9() {
+    return "Why did the golfer bring two pairs of pants? In case he got a hole in one!";
+}
+function joke10() {
+    return "Why did the tomato turn red? Because it saw the salad dressing!";
+    }
+
+
+function getAllJokes() {
+    return [
+        joke1(),
+        joke2(),
+        joke3(),
+        joke4(),
+        joke5(),
+        joke6(),
+        joke7(),
+        joke8(),
+        joke9(),
+        joke10()
+    ];
+}
